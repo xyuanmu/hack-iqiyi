c2374eecbabc37be1f41424b093103a4a7b085b5
xyuanmu <xyuanmu@gmail.com>
2016-01-22 14:34:50 +0800

    initial commit

---
+++ hack-iqiyi.bat
@@ -0,0 +1,4 @@
+@echo off & title Patch iqiyi swf and get enc key
+:do
+"%~dp0/tool/python27.exe" "%~dp0hack-iqiyi.py"
+pause
---
+++ hack-iqiyi.py
@@ -0,0 +1,103 @@
+# coding: utf-8
+
+import os
+import re
+import sys
+import urllib
+import shutil
+import socket
+import subprocess
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import socket
+
+
+PAGE_URL = "http://www.iqiyi.com/v_19rroonq48.html"
+SWF_NAME = "MainPlayer.swf"
+FDIR = os.path.dirname(os.path.abspath(__file__))
+HOST = '127.0.0.1'
+PORT = 8036
+
+
+def download_swf():
+    if os.path.exists(SWF_NAME): os.remove(SWF_NAME)
+    print >> sys.stderr, "Downloading the page %s, please wait..." % PAGE_URL
+    data = urllib.urlopen(PAGE_URL).read()
+    swf_url = re.compile(r'http://[^\'"]+MainPlayer[^.]+\.swf').findall(data)
+    swf_url = swf_url[0]
+    history(swf_url)
+    print 'swf url is %s' % swf_url
+    urllib.urlretrieve(swf_url, SWF_NAME)
+    return os.path.join(FDIR, SWF_NAME)
+
+
+def patch_swf(swf):
+    abc_id = '%s-0' % SWF_NAME.split('.')[0]
+
+    try:
+        os.remove('%s.abc' % abc_id)
+        shutil.rmtree(abc_id)
+    except:
+        pass
+
+    try:
+        subprocess.check_call(['tool/abcexport.exe', '%s' % swf])
+        subprocess.check_call(['tool/rabcdasm.exe', '%s.abc' % abc_id])
+        subprocess.Popen(['tool/patch.exe', '-p0', '-i', '../tool/asasm.patch'], cwd=abc_id).wait()
+        subprocess.check_call(['tool/rabcasm.exe', '%s/%s.main.asasm' % (abc_id, abc_id)])
+        subprocess.check_call(['tool/abcreplace.exe', '%s' % swf, '0', '%s/%s.main.abc' % (abc_id, abc_id)])
+        print 'Patch succeeded!'
+    except:
+        print 'Patch failed!'
+
+
+def history(swf_url):
+    filename = 'history.txt'
+    cache = open(filename).read()
+    his_swf = re.split("\r|\n", cache)
+    if swf_url not in his_swf: open('history.txt', 'a').write(swf_url + "\n")
+
+
+def run_server():
+    #Read flash, put into HTTP response data
+    swf_content = '''
+HTTP/1.x 200 ok
+Content-Type: application/x-shockwave-flash
+
+'''
+    swf_content += open(SWF_NAME, 'rb').read()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((HOST, PORT))
+    sock.listen(100)
+
+    print "Serving HTTP on", HOST, "port", PORT, "..."
+
+    while True:
+        conn, addr = sock.accept()
+        request = conn.recv(1024)
+        method = request.split(' ')[0]
+        try:
+            src  = request.split(' ')[1]
+        except:
+            break
+        if method == 'GET':
+            if src == '/%s' % SWF_NAME:
+                content = swf_content
+            else:
+                content = "Hello World!"
+        elif method == 'POST':
+            form = request.split('\r\n')
+            entry = form[-1]
+            content = entry
+            print "the enc key is: %s" % entry
+        else:
+            continue
+        conn.sendall(content)
+        conn.close()
+
+
+if __name__ == '__main__':
+    swf_path = download_swf()
+    patch_swf(swf_path)
+    run_server()
